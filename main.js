7e692008fb6da3824f5c843839e28c37bad832f7
Szymon Studniarz <szymek1019@gmail.com>
2022-02-13 14:34:35 +0100

    modify elements structure, change transpose handling

--- main.js
+++ main.js
@@ -6,14 +6,16 @@ document.getElementById("createMatrix").addEventListener("click", () => {
     document.querySelector(".content").innerHTML = `<div class="matrixField"></div>
     <button id="transpose">Transponuj</button>`;
     const matrixField = document.querySelector(".matrixField");
-    matrixField.style.width = `${x * 60}px`;
+    //matrixField.style.width = `${x * 60}px`;
+    matrixField.innerHTML += `<div class="matrixSet"></div>`.repeat(x);
+    let target = document.querySelectorAll(".matrixSet");
     for (let i = 0; i < x; ++i){
         for (let j = 0; j < y; ++j){
             let input = document.createElement("input");
             input.className = "matrixValue";
             input.type = "number";
+            target[i].appendChild(input);
             input.value = mat.getValue(i,j);
-            matrixField.appendChild(input);
             input.addEventListener("change", () => {
                 console.log(`${i},${j}: ${input.value}`)
                 mat.setValue(i, j, input.value);
@@ -22,22 +24,43 @@ document.getElementById("createMatrix").addEventListener("click", () => {
     }
     document.getElementById("transpose").addEventListener("click", () => {
         mat.transpose();
-        if (matrixField.style.width === `${ y * 60 }px`) {
-            matrixField.style.width = `${x * 60}px`;   
-        } else {
-            matrixField.style.width = `${y * 60}px`;   
+        const settings = new Array();
+        let style = `
+        .matrixField{
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            flex: 1 1;
         }
-        let inputs = document.querySelectorAll("input");
-        for (let i = 0; i < x; ++i){
-            for (let j = i; j < x * y -1; j += parseInt(x)){
-                console.log(`${i},${j}`);
-                matrixField.insertBefore(matrixField.childNodes[i+j], matrixField.childNodes[i*x+(j/x)+i+1]);
-            }
+        .matrixSet{
+            flex:1;
+            display: flex;
+            flex-direction: column;
         }
-        for (let i = 0; i < x; ++i) {
-            for (let j = 0; j < y; ++j) {
-                inputs[i * x + j].value = mat.getValue(i, j);
-            }
+        `;
+        if (document.querySelector("style").innerHTML.includes(style)) {
+            settings[0] = "wrap";
+            settings[1] = "row";
         }
+        else {
+            settings[0] = "wrap";
+            settings[1] = "column";
+        }
+        document.querySelector("style").innerHTML = `
+        .matrixField{
+            display: flex;
+            flex-direction: row;
+            flex-wrap: ${settings[0]};
+            flex: 1 1;
+        }
+        .matrixSet{
+            flex:1;
+            display: flex;
+            flex-direction: ${settings[1]};
+        }
+        .matrixSet>input{
+            flex: 1;
+        }
+         `;
     });
 });
